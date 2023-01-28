0381544cd7f3b2e11e251cd8f7dda7bdf063ba67
dhanushka365 <technohub10000@gmail.com>
2023-01-28 14:15:55 +0530

    query

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/html')
+def get_html():
+    return render_template("index.html")
+
+
+@app.route('/qs')
+def get_qs():
+    if request.args:
+        req = request.args
+        return " ".join(f"{k}:{v}" for k,v in req.items())
+    return "No query"
+
 if __name__ == '__main__':
-    app.run(port=5004)
+    app.run(port=5004,debug=True)
